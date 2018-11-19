dc628a897673ebbe96beec4231a4809a47c9a60a
Anthony Marques <anthony@armorica.gk>
2018-11-19 14:58:32 +0200

    added Material UI component

--- dropdown/src/App.js
+++ dropdown/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import DropDown from "./dropDown/dropdown";
-import MaterialdropDown from "./materialdropDown/materialdropDown";
+import SimpleMenu from "./materialdropDown/materialdropDown";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>DropDown menu practice</p>
-        <DropDown />
-        <br />
-        <MaterialdropDown />
+          <p>DropDown menu practice</p>
+          <DropDown />
+          <br />
+          <SimpleMenu  />
       </div>
     );
   }
--- dropdown/src/materialdropDown/materialdropDown.js
+++ dropdown/src/materialdropDown/materialdropDown.js
@@ -1,30 +1,46 @@
-import React, { Component } from "react";
-import DropDownMenu from 'material-ui/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-import "./materialdropDown.css";
+class SimpleMenu extends React.Component {
+  state = {
+    anchorEl: null,
+  };
 
-const items = [];
-for (let i = 0; i < 100; i++ ) {
-  items.push(<MenuItem value={i} key={i} primaryText={`Item ${i}`} />);
-}
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
 
-
-class MaterialdropDown extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 10};
-  }
-
-  handleChange = (event, index, value) => this.setState({value});
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
 
   render() {
+    const { anchorEl } = this.state;
+
     return (
-      <DropDownMenu maxHeight={300} value={this.state.value} onChange={this.handleChange}>
-        {items}
-      </DropDownMenu>
+      <div>
+        <Button
+          aria-owns={anchorEl ? 'simple-menu' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          Open Menu
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+        >
+          <MenuItem onClick={this.handleClose}>Menu item 1</MenuItem>
+          <MenuItem onClick={this.handleClose}>Menu item 2</MenuItem>
+          <MenuItem onClick={this.handleClose}>Menu item 3</MenuItem>
+        </Menu>
+      </div>
     );
   }
 }
 
-export default MaterialdropDown;
+export default SimpleMenu;
